0765a260c80d5c8f472ffa3cccc2eab000f0b1f0
mizmizo <mizmizogb@gmail.com>
2020-05-19 23:06:39 +0900

    add err_handler.js, log&show when Graph API returns error

---
+++ app/lib/err_handler.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const common_errmessage = 'Instagram Graph APIエラーを検知しました。';
+const api_errmessage_header = 'API error: ';
+const sustainable_message = '分析を中止します。';
+const critical_message = 'アプリを終了します。';
+const transient_message = '一時的なエラーが発生しています。時間をおいて再度お試しください。';
+const api_errcode = {
+    803:{sustainable:true,
+         message:'ビジネスアカウントIDが間違っています。'},
+    190:{sustainable:true,
+         message:'アクセストークンが間違っています。'},
+    100:{sustainable:true,
+         message:'アクセストークンの設定が間違っています。'},
+    2500:{sustainable:false,
+          message:'アプリサポートにご連絡ください。'},
+    18:{sustainable:true,
+          message:'分析するハッシュタグの数を減らしてお試しください。'}
+};
+
+// code : 0 -> Built-in error, 1 -> App-sequence error, other -> API error
+class AppError extends Error {
+    constructor(sustainable, code, ...params) {
+        // Pass remaining arguments (including vendor specific ones) to parent constructor
+        super(...params);
+
+        // Maintains proper stack trace for where our error was thrown (only available on V8)
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, AppError);
+        }
+
+        this.sustainable = sustainable;
+        this.code = code;
+        this.message += sustainable ? '\n' + sustainable_message :  '\n' + critical_message;
+    }
+}
+
+// If json contains 'error' field, throw AppError.
+function checkAPIRes (json) {
+    if(json.error){
+        let message = common_errmessage + '\n';
+        let sustainable = true; // treat non-listed errors as non-critical
+        // if the error-code is registered to api_errcode, add help-message
+        if(api_errcode[json.error.code]){
+            sustainable = api_errcode[json.error.code].sustainable;
+
+            message += api_errcode[json.error.code].message + '\n';
+        }
+
+        // copy API error
+        message += '\n' + api_errmessage_header + json.error.message + '\n' + json.error.error_user_msg;;
+
+        throw new AppError(sustainable, json.error.code, message);
+    }
+}
+
+module.exports = {
+    AppError,
+    checkAPIRes
+}
